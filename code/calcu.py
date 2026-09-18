28ee70c78477782ac20737d201b0b9a38dd29863
Jhonnsson Santiago Henao Quintero <sathenao2002@gmail.com>
2026-09-17 19:07:46 -0600

    Actualice las funciones de la calculadora en calcu.py

--- code/calcu.py
+++ code/calcu.py
@@ -7,3 +7,11 @@ def resta(valor1, valor2);
 def multi(valor1, valor2);
     resta valor1 * valor2
 
+resultado_suma = suma(2, 5)
+resultado_resta = resta(2, 5)
+resultado_multi = multi(2, 5)
+
+
+print(f"Resultado suma: {resultado_suma}")
+print(f"Resultado resta: {resultado_resta}")
+print(f"Resultado multiplicasion: {resultado_multi}")
