8bbac8b3bdc15e91a5b1a53db77785e7cb6c3cba
Jhonnsson Santiago Henao Quintero <sathenao2002@gmail.com>
2026-09-24 19:14:24 -0600

    correcion_1

--- taller_01/desarrollo_01.py
+++ taller_01/desarrollo_01.py
@@ -1,14 +1,22 @@
 import threading
 import time
 
-
 def tarea(n):
     time.sleep(1)
 
+# 1. Corrección del signo de asignación
+inicio = time.perf_counter() 
+hilos = []
 
-inicio = time.perf_counter()
+# 2. Creamos y arrancamos todos los hilos para evitar errores en la ejecusion de tiempo
 for i in range(3):
     hilo = threading.Thread(target=tarea, args=(i,))
+    hilos.append(hilo)
     hilo.start()
+
+# 3. Esperamos a que todos terminen fuera del bucle del pricipio
+for hilo in hilos:
     hilo.join()
+
+# 4. Imprimimos el tiempo trancurrido
 print(f"{time.perf_counter() - inicio:.1f} s")
